da747a584fcd5da9635f4ef77d2a33150ef3cc34
Kodik77rus <plyhach@gmail.com>
2022-01-12 04:32:29 +0300

    fix

--- internal/fin-spy-tg-bot/models/user.go
+++ internal/fin-spy-tg-bot/models/user.go
@@ -2,12 +2,7 @@ package models
 
 type User struct {
 	Watchlist []Watchlist
-<<<<<<< HEAD
 	Id        uint   `json:"id" gorm:"primaryKey"`
 	UserName  string `json:"first_name"`
-=======
-	Id        uint   `json: "id;"gorm: "primaryKey"`
-	UserName  string `json: "first_name"`
->>>>>>> b7608121ac40f6ee6acc1f4b47dcbe84db968401
 	Language  string `json:"language_code"`
 }
--- internal/fin-spy-tg-bot/models/watch-list.go
+++ internal/fin-spy-tg-bot/models/watch-list.go
@@ -2,13 +2,7 @@ package models
 
 type Watchlist struct {
 	User   User
-<<<<<<< HEAD
 	UserId uint   `json:"UserId"`
 	Price  uint   `json:"regularMarketPrice"`
 	Ticker string `json:"ticker"`
-=======
-	UserId uint   `json :"UserId"`
-	Price  uint   `json :"regularMarketPrice"`
-	Ticker string `json :"ticker"`
->>>>>>> b7608121ac40f6ee6acc1f4b47dcbe84db968401
 }
