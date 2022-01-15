56de9d3d76e0426989dbfa9a2768b58652586694
Kodik77rus <plyhach@gmail.com>
2022-01-16 02:28:10 +0300

    create base methods for user model

--- storage/users-repository.go
+++ storage/users-repository.go
@@ -1,6 +1,30 @@
 package storage
 
-//Instance of User model
-type UserRepository struct {
-	storage *Storage
+import (
+	"github.com/Kodik77rus/fin-spy-tg-bot/internal/fin-spy-tg-bot/models"
+	"gorm.io/gorm"
+)
+
+var user *models.User
+
+func (st *Storage) CreateUser(user *models.User) error {
+	if err := st.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (st *Storage) FindUser(id uint) (*gorm.DB, error) {
+	result := st.db.First(&user, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return result, nil
+}
+
+func (st *Storage) UpdateUser(user *models.User) error {
+	if err := st.db.Save(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
