2c3ce7eb4881dc090a176767480fe530bd251fda
Kodik77rus <plyhach@gmail.com>
2022-01-22 20:53:32 +0300

    create new user field

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/jasonlvhit/gocron v0.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- internal/fin-spy-tg-bot/models/user.go
+++ internal/fin-spy-tg-bot/models/user.go
@@ -1,8 +1,12 @@
 package models
 
+import (
+	"github.com/lib/pq"
+)
+
 type User struct {
-	Watchlist []Watchlist
-	Id        uint   `json:"id" gorm:"primaryKey"`
-	UserName  string `json:"first_name" gorm:"NOT NULL"`
-	Language  string `json:"language_code" gorm:"NOT NULL"`
+	Id       uint           `json:"id" gorm:"primaryKey"`
+	UserName string         `json:"first_name" gorm:"NOT NULL"`
+	Language string         `json:"language_code" gorm:"NOT NULL"`
+	Markets  pq.StringArray `json:"markets" gorm:"type:text[]" `
 }
