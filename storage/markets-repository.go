e8b9219b4634af0b4f2b8b92b68d93f75b8d0b53
Kodik77rus <plyhach@gmail.com>
2022-02-03 23:58:32 +0300

    get all markets db query

--- storage/markets-repository.go
+++ storage/markets-repository.go
@@ -8,10 +8,26 @@ const pageSize = 2
 
 var markets []models.Market
 
-func (st *Storage) GetAllMarkets(page int) ([]models.Market, error) {
-	result := st.db.Limit(pageSize).Offset(pageSize * (page - 1)).Find(&markets)
+type MarketResponse struct {
+	Markets []models.Market
+	Count   int
+}
+
+func (st *Storage) GetAllMarkets(page int) (*MarketResponse, error) {
+	result := st.db.Select(
+		"name",
+		"code",
+		"mic",
+		"location",
+		"country",
+		"city",
+		"delay",
+		"hour",
+	).Limit(pageSize).Offset(pageSize * (page - 1)).Find(&markets)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	return markets, nil
+
+	response := MarketResponse{Markets: markets, Count: int(result.RowsAffected)}
+	return &response, nil
 }
